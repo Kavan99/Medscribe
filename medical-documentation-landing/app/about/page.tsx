b82103fac6592c7a5a5de9da2db364ce57ef3c50
Raghu Nandan <144773863+Raghu6798@users.noreply.github.com>
2025-04-11 11:29:32 +0530

    Update page.tsx

--- medical-documentation-landing/app/about/page.tsx
+++ medical-documentation-landing/app/about/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { FileText, Linkedin } from "lucide-react"
+import {  Linkedin } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Metadata } from "next"
 import { FloatingCta } from "../components/floating-cta"
@@ -23,7 +23,7 @@ export default function AboutPage() {
             <div className="relative">
               <div className="absolute -inset-1 bg-gradient-to-r from-blue-600 to-purple-600 rounded-full opacity-70 group-hover:opacity-100 blur-sm group-hover:blur transition duration-200"></div>
               <div className="relative bg-gray-900 rounded-full p-2">
-                <FileText className="h-6 w-6 text-blue-400 group-hover:text-blue-300 transition-colors" />
+                <img src="/logo.png" alt="MediScribe Logo" className="h-10 w-19" />
               </div>
             </div>
             <span className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-400">
